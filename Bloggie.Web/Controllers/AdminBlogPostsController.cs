3b8481f9bf382f132c1ef7ab423f3b8ab1f088e5
Yossi Karasik <yosikari@gmail.com>
2023-06-04 12:06:58 +0300

    Add post form, with selected tag

---
+++ Bloggie.Web/Controllers/AdminBlogPostsController.cs
@@ -0,0 +1,31 @@
+﻿using Bloggie.Web.Models.ViewModels;
+using Bloggie.Web.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Bloggie.Web.Controllers
+{
+	public class AdminBlogPostsController : Controller
+	{
+		private readonly ITagRepository tagRepository;
+
+		public AdminBlogPostsController(ITagRepository tagRepository)
+		{
+			this.tagRepository = tagRepository;
+		}
+
+		[HttpGet]
+		public async Task<IActionResult> Add()
+		{
+			//get tags from repository
+			var tags = await tagRepository.GetAllAsync();
+
+			var model = new AddBlogPostRequest
+			{
+				Tags = tags.Select(x => new SelectListItem { Text = x.Name, Value = x.Id.ToString() })
+			};
+
+			return View(model);
+		}
+	}
+}
