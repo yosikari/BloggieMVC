bcc270fb188765f69bf65264e82c8610a1083fe9
Yossi Karasik <yosikari@gmail.com>
2023-06-05 13:22:34 +0300

    Create Image upload API using cloudinary (tested with postman)

---
+++ Bloggie.Web/Controllers/ImagesController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bloggie.Web.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class ImagesController : ControllerBase
+	{
+	}
+}
---
+++ Bloggie.Web/Repositories/IImageRepository.cs
@@ -0,0 +1,6 @@
+﻿namespace Bloggie.Web.Repositories
+{
+	public interface IImageRepository
+	{
+	}
+}
