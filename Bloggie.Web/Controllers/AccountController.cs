eb8d95a85245646d2ffeb138c3f86e90021913e0
Yossi Karasik <yosikari@gmail.com>
2023-06-06 17:40:30 +0300

    Add user register functionality with required password upercase/digit/unique/min-lenght

---
+++ Bloggie.Web/Controllers/AccountController.cs
@@ -0,0 +1,47 @@
+﻿using Bloggie.Web.Models.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bloggie.Web.Controllers
+{
+	public class AccountController : Controller
+	{
+		private readonly UserManager<IdentityUser> userManager;
+
+		public AccountController(UserManager<IdentityUser> userManager)
+        {
+			this.userManager = userManager;
+		}
+
+        [HttpGet]
+		public IActionResult Register()
+		{
+			return View();
+		}
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel registerViewModel)
+		{
+			var identityUser = new IdentityUser
+			{
+				UserName = registerViewModel.Username,
+				Email = registerViewModel.Email,
+			};
+
+			var identityResult = await userManager.CreateAsync(identityUser, registerViewModel.Password);
+			if (identityResult.Succeeded)
+			{
+				// Assign this user the "User" role
+				var roleIdentityResult = await userManager.AddToRoleAsync(identityUser, "User");
+				if (roleIdentityResult.Succeeded)
+				{
+					//Show success notification
+					return RedirectToAction("Register");
+				}
+			}
+
+			//Show error notification
+			return View();
+		}
+    }
+}
--- Bloggie.Web/Data/AuthDbContext.cs
+++ Bloggie.Web/Data/AuthDbContext.cs
@@ -6,7 +6,7 @@ namespace Bloggie.Web.Data
 {
 	public class AuthDbContext : IdentityDbContext
 	{
-		public AuthDbContext(DbContextOptions options) : base(options)
+		public AuthDbContext(DbContextOptions<AuthDbContext> options) : base(options)
 		{
 		}
 
